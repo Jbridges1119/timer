866949f28d45fef970f655dd039646aab066d7af
Jeff Bridges <jbridges1119@gmail.com>
2022-06-07 15:15:36 +0000

    program to cause a beep at specified times

---
+++ timer1.js
@@ -0,0 +1,19 @@
+const args = process.argv.slice(2);
+const arr = args.sort((a, b) => a - b)
+console.log(arr)
+
+if (arr.length === 0) {
+} else {
+  for (let number of arr) {
+    if (isNaN(number) || number < 0) {
+    } else {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, number*1000)
+    }
+  }
+}
+
+
+
+//if(arr.length === 0) {}
