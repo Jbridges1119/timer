14aa70fcf8c01930856f24de84285c614de9c0de
Jeff Bridges <jbridges1119@gmail.com>
2022-06-08 01:44:08 +0000

    refactored code

--- timer1.js
+++ timer1.js
@@ -1,14 +1,13 @@
 const args = process.argv.slice(2);
-const arr = args.sort((a, b) => a - b);
 
-if (arr.length === 0) {
-} else {
-  for (let number of arr) {
-    if (isNaN(number) || number < 0) {
-    } else {
-      setTimeout(() => {
-        process.stdout.write('\x07');
-      }, number * 1000);
-    }
+for (let number of args) {
+  if (!isNaN(number) || number > 0) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, number * 1000);
   }
-}
+};
+
+
+
+
